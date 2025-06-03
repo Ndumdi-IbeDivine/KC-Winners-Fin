40169915741df5754071aea9364b5ac08052f444
Ndumdi-IbeDivine <dvineonyi@gmail.com>
2025-06-03 05:18:56 -0700

    Made changes to the CSS including responsiveness Added navigate to account page when user is registered successfully

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/RegistrationPage';
+import AccountPage from './pages/AccountPage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<RegisterPage />} />
+        <Route path="/account" element={<AccountPage />} />
       </Routes>
     </Router>
   );
